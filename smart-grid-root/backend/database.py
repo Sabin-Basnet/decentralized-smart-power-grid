7b04190c4f3ae19e2c4ecf62aed69e8809815af3
Sabin-Basnet <sabin9824566009@gmail.com>
2026-06-19 15:12:26 +0545

    database Created

--- smart-grid-root/backend/database.py
+++ smart-grid-root/backend/database.py
@@ -1,13 +1,14 @@
 import sqlite3
 
-conn = sqlite3.connect("backend_data.db")
+database_name = 'backend_data.db'
+conn = sqlite3.connect(database_name)
 cursor = conn.cursor()
 
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS Accounts(
         account_id TEXT PRIMARY_KEY,
         token_balance REAL,
-        cummulative_energy_month REAL,
+        cumulative_energy_month REAL,
         system_status TEXT
     )
 """)
@@ -25,3 +26,43 @@ cursor.execute("""
 print("Database created...")
 conn.commit()
 conn.close()
+
+
+def get_info_by_account_id(account_id):
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT token_balance, cumulative_energy_month, system_status FROM Accounts WHERE account_id = ?", (account_id,))
+    account = cursor.fetchone()
+    conn.close()
+    return account
+
+
+#just checking 
+# cursor.execute("""
+# INSERT INTO Accounts (account_id, token_balance, cumulative_energy_month, system_status) VALUES('mine',300, 45, 'this is good')
+# """)
+# conn.commit()
+# get_info_by_account_id('mine')
+
+def update_account(account_id, new_balance, updated_units, current_status):
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE Accounts 
+        SET token_balance = ?, cumulative_energy_month = ?, system_status = ?
+        WHERE account_id = ?
+    """, (new_balance, updated_units, current_status, account_id))
+    conn.commit()
+    conn.close()
+
+
+def append_meter_data(account_id, current_phase, current_neutral, mismatch):
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO Meter_History (account_id, timestamp, load_phase, load_neutral, mismatch)
+        VALUES (?, datetime('now'), ?, ?, ?)
+    """, (account_id, current_phase, current_neutral, mismatch))
+    conn.commit()
+    conn.close()
+
---
+++ smart-grid-root/backend/main.py
@@ -0,0 +1,3 @@
+from fastapi import FastAPI # type: ignore
+
+app = FastAPI()
